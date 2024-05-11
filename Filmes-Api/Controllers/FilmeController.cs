40386495bff2e0dc5ffe91515a0e0cda39d4e462
afrain <calixtoafrain@gmail.com>
2024-05-11 13:45:56 -0300

    Validando parâmetros recebidos

---
+++ Filmes-Api/Controllers/FilmeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes_Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController
+    {
+        [HttpGet]
+        public string AdicionaFilme ()
+        {
+            return "FilmeAdicionado";
+        }
+    }
+}
--- Filmes-Api/Models/Filme.cs
+++ Filmes-Api/Models/Filme.cs
@@ -1,9 +1,16 @@
-﻿namespace Filmes_Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filmes_Api.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O titulo do filme é obrigatório")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
         public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
